077fa2c11e4fcc22d6046b9137fa2d9c896925b4
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-08-22 00:13:09 +0200

    scope actions work on segments now

--- lib/aygabtu/scope_actor.rb
+++ lib/aygabtu/scope_actor.rb
@@ -11,43 +11,64 @@ module Aygabtu
     end
 
     def pass(pass_data)
-      pass_data = @scope.pass_data.merge(pass_data)
+      each_empty_scope_segment do |scope, generator|
+        generator.generate_pending_no_match_failing_example # @TODO
+      end
 
-      each_route do |route|
+      each_scope_segment_and_route do |scope, generator, route|
+        pass_data = @scope.pass_data.merge(pass_data)
+
         route.touch!
         generator.generate_example(route, pass_data)
-      end or generator.generate_pending_no_match_failing_example # @TODO
+      end
     end
 
     def ignore(reason)
       raise "Reason for ignoring must be a string" unless reason.is_a?(String)
 
-      each_route(&:touch!)
+      each_empty_scope_segment do |scope, generator|
+        generator.generate_pending_no_match_failing_example # @TODO
+      end
+
+      each_scope_segment_and_route do |scope, generator, route|
+        route.touch!
+      end
     end
 
     def pend(reason)
-      each_route do |route|
+      each_empty_scope_segment do |scope, generator|
+        generator.generate_pending_no_match_failing_example
+      end
+
+      each_scope_segment_and_route do |scope, generator, route|
         route.touch!
         generator.generate_pending_example(route, reason)
-      end or generator.generate_pending_no_match_failing_example
+      end
     end
 
     private
 
-    def generator
-      @generator ||= Generator.new(@scope, @example_group)
+    def each_scope_segment_and_route
+      segments_generators_routes.each do |segment, generator, routes|
+        routes.each { |route| yield segment, generator, route }
+      end
     end
 
-    def each_route
-      match = false
-      @routes.each do |route|
-        if @scope.matches_route?(route)
-          match = true
-          yield route
-        end
-      end
+    def each_empty_scope_segment
+      segments_generators_routes.each do |segment, generator, routes|
+        next unless routes.empty?
 
-      match
+        yield segment, generator
+      end
+    end
+
+    def segments_generators_routes
+      @segments_and_generators ||= @scope.segments.map do |segment|
+        generator = Generator.new(segment, @example_group)
+        routes = @routes.select { |route| segment.matches_route?(route) }
+
+        [segment, generator, routes]
+      end
     end
   end
 end
