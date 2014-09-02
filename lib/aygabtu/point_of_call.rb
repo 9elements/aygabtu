cb7dc9f7cd021047aafb8ab923be982eed4ee16b
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-09-02 21:01:00 +0200

    don't use a top-level constant. also made gem root calculation less brittle

--- lib/aygabtu/point_of_call.rb
+++ lib/aygabtu/point_of_call.rb
@@ -1,18 +1,28 @@
 require_relative 'scope/base'
 
-GEM_ROOT = Pathname(__FILE__).parent.to_s
-
 module Aygabtu
+
   module PointOfCall
     extend self
 
     def point_of_call
-      caller.drop_while { |point| point.start_with?(GEM_ROOT) }.first
+      caller.drop_while { |point| point.start_with?(PointOfCall.gem_root) }.first
     end
 
     def file_and_line_at_point_of_call
       filename, line_and_context = point_of_call.split(':', 2)
       [filename, line_and_context.to_i] # make use of the fact to_i tolerates being passed a string only _beginning_ with a number
+    end
+
+    def self.gem_root
+      return @gem_root if @gem_root
+
+      path = Pathname(__FILE__)
+      while new_path = path.parent and new_path.to_s.include?('lib/aygabtu')
+        path = new_path
+      end
+
+      @gem_root= path.to_s
     end
   end
 end
