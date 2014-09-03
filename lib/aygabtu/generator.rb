a402492d684ff03fcafd576ea4dd5ca631da139d
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-09-03 11:04:32 +0200

    in RSpec 3, pending examples need to fail

--- lib/aygabtu/generator.rb
+++ lib/aygabtu/generator.rb
@@ -30,7 +30,16 @@ module Aygabtu
     end
 
     def pending_example(route, reason)
-      "it(#{route.example_message.inspect}) { pending #{reason.to_s.inspect} }"
+      body = [
+        "pending #{reason.to_s.inspect}"
+      ]
+      if ::RSpec::Core::Version::STRING.split('.').first == '3'
+        # RSpec totally changed their mind on pending. We should be using skip here, but apparently skipped
+        # examples do not take or report any reason for skipping, and we want the reason to appear
+        # at least in the documentation format.
+        body << 'raise("rspec_core treats pending examples as failing unless it _would_ fail")'
+      end
+      "it(#{route.example_message.inspect}) { #{body.join('; ')} }"
     end
 
     def no_match_failing_example(action)
