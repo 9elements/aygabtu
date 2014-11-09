717ba5eac87fb73f6a118491374628bc47bf5175
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-11-09 21:37:55 +0100

    skip bundled tests in spec task

--- Rakefile
+++ Rakefile
@@ -2,7 +2,9 @@ require "bundler/gem_tasks"
 
 begin
   require 'rspec/core/rake_task'
-  RSpec::Core::RakeTask.new(:spec)
+  RSpec::Core::RakeTask.new(:spec) do |t|
+    t.rspec_opts = "--tag ~bundled"
+  end
 rescue LoadError
 end
 
--- spec/lib/route_wrapper_spec.rb
+++ spec/lib/route_wrapper_spec.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
 end
 
 
-describe "RouteWrapper" do
+describe "RouteWrapper", bundled: true do
   include Aygabtu::RSpec.example_group_module
 
   describe "test mechanism for identifying routes independently of controller, name and action" do
