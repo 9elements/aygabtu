5e0babd7c6938f52a8bcc270b02f039f21f138ee
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-11-16 22:05:09 +0100

    reorganize integration test wrapper

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -9,13 +9,9 @@ require 'support/invokes_rspec'
 #require 'pry'
 #require 'pry-byebug'
 
-describe "foo" do
-  context "foo" do
+describe "behaviour under different gem versions" do
+  shared_examples_for "integration-testing aygabtu" do
     include InvokesRspec
-
-    def gemfile_env
-      { 'RAILS_VERSION' => '4.2.0.beta2'}
-    end
 
     it "passes RouteWrapper tests" do
       expect(rspec_result('spec/lib/route_wrapper_spec.rb')).to contain_only_passed_examples
@@ -28,6 +24,14 @@ describe "foo" do
     it "passes example anatomy tests" do
       expect(rspec_result('spec/example_spec.rb')).to contain_only_passed_examples
     end
+  end
+
+  context "the currently only gem combination, more to follow" do
+    def gemfile_env
+      { 'RAILS_VERSION' => '4.2.0.beta2'}
+    end
+
+    include_examples "integration-testing aygabtu"
   end
 end
 
